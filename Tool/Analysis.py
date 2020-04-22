157527adaa46c6a2f4107e86b74b32a08596304f
xushunjie@zhuojianchina.com <xushunjie@zhuojianchina.com>
2020-04-22 20:29:16 +0800

    调整排版 + 图表

--- Tool/Analysis.py
+++ Tool/Analysis.py
@@ -2,6 +2,7 @@ from pyecharts import options as opts
 from pyecharts.charts import PictorialBar, Line, Bar
 from pyecharts.globals import SymbolType
 import pandas as pd
+from pyecharts.commons.utils import JsCode
 import re
 
 
@@ -66,7 +67,7 @@ class province_rt:
         tem_data = list(tem_data)
 
         # 降水和温度柱状图
-        pictorialbar = PictorialBar(init_opts=opts.InitOpts(width="310px", height="250px", bg_color="#12406d"))
+        pictorialbar = PictorialBar(init_opts=opts.InitOpts(width="100%", height="100%", bg_color="#12406d"))
         pictorialbar.add_xaxis(weather_info_ctiy)
         pictorialbar.add_yaxis(
             "降水量", weather_data,
@@ -110,4 +111,102 @@ class province_rt:
         return pictorialbar
 
 
+class province_rt2:
 
+    def __init__(self, csv_path):
+        self.provincial = pd.read_csv('{}/data/provincial_capital.csv'.format(csv_path))  # 读取省会信息
+        self.china_city_code = pd.read_csv('{}/data/china-city-list.csv'.format(csv_path))  # 读取城市 id 信息
+        self.weather = pd.read_csv("{}/data/weather_data.csv".format(csv_path))  # 读取天气信息
+
+    def trans_tem(self, tem):
+        # 获取温度信息
+        rege = r'-?(\d+)℃/-?(\d+)℃'
+        tmp_tem = re.match(rege, tem)
+        mid_tem = (int(tmp_tem.group(1)) + int(tmp_tem.group(2))) / 2
+        return mid_tem
+
+    # 转换降水信息
+    def check_weather(self, wea):
+        # 转换天气变量，数值越多，说明降水概率越大
+        weather_dict = {
+            "snow": 100,
+            "rain": 80,
+            "cloud": 50,
+            "overcast": 60,
+            "sun": 20
+        }
+        if wea[-1:] == '晴':
+            wea = weather_dict['sun']
+        elif wea[-1:] == '云':
+            wea = weather_dict['cloud']
+        elif wea[-1:] == '雨':
+            wea = weather_dict['rain']
+        elif wea[-1:] == '阴':
+            wea = weather_dict['overcast']
+        return wea
+
+    def render(self):
+
+        # 提取省会城市 id
+        provincial_data = pd.DataFrame()
+        for i in self.provincial['city'].values.tolist():
+            for j in self.china_city_code['City_CN'].values.tolist():
+                if j == i:
+                    provincial_data = pd.concat([self.china_city_code[self.china_city_code['City_CN'] == j], provincial_data])
+
+        # 按照城市分组
+        wea_group = self.weather.groupby('city').apply(lambda x: x[:])
+        # 提取当天天气信息
+        weather_info = wea_group[wea_group['time'] == '周五（13日）']
+        # 城市信息
+        weather_info_ctiy = weather_info['city'].values.tolist()
+        weather_info_ctiy = list(weather_info_ctiy)
+
+
+        # 获取降水和温度信息
+        weather_data = map(self.check_weather, weather_info['wea'].values.tolist())
+        weather_data = list(weather_data)
+        tem_data = map(self.trans_tem, weather_info['tem'].values.tolist())
+        tem_data = list(tem_data)
+
+        # 在双轴图中查看
+        bar = Bar(init_opts=opts.InitOpts(width="100%", height="100%", bg_color="#12406d"))
+        bar.add_xaxis(weather_info_ctiy)
+        bar.add_yaxis("", weather_data)
+        bar.extend_axis(
+            yaxis=opts.AxisOpts(
+                axislabel_opts=opts.LabelOpts(formatter="{value} °C", color="white"), interval=5
+            )
+        )
+        bar.set_global_opts(
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-30, color="white"), is_show=True),
+            yaxis_opts=opts.AxisOpts(
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                axisline_opts=opts.AxisLineOpts(
+                    linestyle_opts=opts.LineStyleOpts(opacity=0)
+                ),
+                axislabel_opts=opts.LabelOpts(formatter="{value}%\n降水概率", color="white", font_size=10)
+            ),
+            datazoom_opts=opts.DataZoomOpts()
+        )
+        bar.set_series_opts(itemstyle_opts={
+            "normal": {
+                "color": JsCode("""new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                            offset: 0,
+                            color: 'rgba(0, 244, 255, 1)'
+                        }, {
+                            offset: 1,
+                            color: 'rgba(0, 77, 167, 1)'
+                        }], false)"""),
+                "barBorderRadius": [30, 30, 30, 30],
+                "shadowColor": 'rgb(0, 160, 221)',
+            }, "color": "white"
+        })
+        line = Line()
+        line.add_xaxis(weather_info_ctiy).add_yaxis("", tem_data, yaxis_index=1)
+        line.set_series_opts(label_opts=opts.LabelOpts(is_show=True, color="orange"))
+
+        bar.overlap(line)
+        # line.overlap(bar)
+
+        return bar
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from Tool import ChartTool,Analysis
+
 import os
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ app = Flask(__name__)
 def index():
     project_path = os.path.dirname(os.path.abspath(__file__))
     china_map = ChartTool.geo()
-    chart1 = Analysis.province_rt(project_path)
+    chart1 = Analysis.province_rt2(project_path)
+
     return render_template("index.html",
                            map=china_map.render_embed(),
                            chart1=chart1.render().render_embed()
